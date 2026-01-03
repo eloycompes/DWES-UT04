169a54c02b126224538775c6bf04d8cf0715bad0
Eloy Compés <eloycompes@gmail.com>
2026-01-03 18:13:24 +0100

    Tercer commit: tabla de usuarios creada, listado funcional en navegador

--- 04-django/ejercicio04_Tareas/urls.py
+++ 04-django/ejercicio04_Tareas/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from usuarios.views import usuario_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('usuario/', include("usuarios.urls")),
+    path('usuarios/', include("usuarios.urls")),
 ]
--- 04-django/usuarios/views.py
+++ 04-django/usuarios/views.py
@@ -5,3 +5,12 @@ def lista_usuarios(request):
     usuarios = Usuario.objects.all()
     return render(request, "usuarios/lista_usuarios.html", {"usuarios": usuarios})
 
+def crear_usuario(request):
+    if request.method == "POST": # Nos envian datos desde el formulario para crear un usuario
+        form = UsuarioForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("lista_usuarios") # o la vista que muestra todos
+    else:
+        form = UsuarioForm()
+    return render(request, "usuarios/crear_usuario.html", {"form": form})
